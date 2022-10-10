e94c820a48249cc7d4a19e1a6657c82065539dc5
jailsonh3 <jailsonh3@gmail.com>
2022-10-10 01:09:03 -0300

    Finish challenger 01 - Iginit ReactJS

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,32 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
+
+    const data = {
+      id: Math.floor(Math.random() * 10000 + 1),
+      title: newTaskTitle,
+      isComplete: false
+    } as Task;
+
+    setTasks([...tasks, data]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const updateTasks = tasks.map(task => {
+      return task.id === id ? {...task, isComplete: !task.isComplete} : task
+    })
+
+    setTasks(updateTasks)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const tasksFilter = tasks.filter(task => task.id !== id);
+    setTasks(tasksFilter)
+    
   }
 
   return (
